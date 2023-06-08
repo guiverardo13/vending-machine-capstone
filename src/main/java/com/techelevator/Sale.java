5afbae2d67fa35f3d9ce1f3e8fa0a5eb8875f532
e-fanto <elise.m.fanto@gmail.com>
2023-06-08 19:26:43 -0400

    Last push tonight

--- src/main/java/com/techelevator/Sale.java
+++ src/main/java/com/techelevator/Sale.java
@@ -1,16 +1,21 @@
 package com.techelevator;
 //this will contain a method for making change into proper coin amounts. {i'm excited to try and figure this one out!}
 public class Sale {
-    //Constructor
+    private int changeTotal;
+
+
     public Sale(){
 
     }
 
+    private String getChangeCoins(int balance, int totalCost){
+        changeTotal = balance - totalCost;
+        int[] coins = new int[4];//{Quarters,Dimes,Nickels,Pennies}
+        //COIN LOGIC
 
 
-    public String getChangeCoins(int changeTotal){
 
 
-
+        return "";
     }
 }
