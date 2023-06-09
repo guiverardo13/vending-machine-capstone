fdc64c25767579d360d1d77447e7051bcf761a26
e-fanto <elise.m.fanto@gmail.com>
2023-06-09 17:52:50 -0400

    stuff

--- src/main/java/com/techelevator/Slot.java
+++ src/main/java/com/techelevator/Slot.java
@@ -6,53 +6,30 @@ import java.util.Scanner;
 
 public class Slot {
 
-    private String pathName = "vendingmachine.csv";
-    //SlotMap <String(SlotKEy),Map<Item , int (quantity)
+    private final int STARTING_INVENTORY = 5;
+    public int inventory;
     private String[] lineParts;
+    public Item currentItem;
 
-    public String buildItem() throws MalformedItemException {
-
-        SlotMap map = new SlotMap();
-        Item currentItem = null;
-        File fileName = new File(pathName);
-        SlotMap itemList = null;
-
-
-        try (Scanner fileReader = new Scanner(fileName)) {
-
-            while (fileReader.hasNextLine()) {
-                String line = fileReader.nextLine();
-                currentItem = parseLine(line);
-
-                System.out.println(currentItem.getKeyCode() + " " + currentItem.getName() + " " + currentItem.getPrice());
-            }
-
-        } catch (FileNotFoundException e) {
-            throw new MalformedItemException();
-        }
-        return "";
-    }
-    private Item parseLine(String line) {
+    public Slot(String line) {
         lineParts = line.split("\\|");
         String key = lineParts[0];
         String name = lineParts[1];
         double price = Double.parseDouble(lineParts[2]);
         String type = lineParts[3];
 
-        if (type.equals("Chip")){
-            return new Chip(key, name, price);
-        } else if (type.equals("Drink")){
-            return new Drink(key, name, price);
-        } else if (type.equals("Gum")){
-            return new Gum(key, name, price);
-        } else if (type.equals("Candy")){
-            return new Candy(key, name, price);
-        } else {return null;}
+        if (type.equals("Chip")) {
+            currentItem = new Chip(key, name, price);
+        } else if (type.equals("Drink")) {
+            currentItem = new Drink(key, name, price);
+        } else if (type.equals("Gum")) {
+            currentItem = new Gum(key, name, price);
+        } else if (type.equals("Candy")) {
+            currentItem = new Candy(key, name, price);
 
+            inventory = STARTING_INVENTORY;
+        }
 
     }
-    public String getCode(){
-        return lineParts[0];
-    }
+
 }
-               // map.addItem(currentSlot);//////<------- might want to loop and add to a different MAP outside of scanner
--- src/main/java/com/techelevator/SlotMap.java
+++ src/main/java/com/techelevator/SlotMap.java
@@ -1,19 +1,44 @@
 package com.techelevator;
 
-
+import java.util.Scanner; //new
+import java.io.File; //new
+import java.io.FileNotFoundException; //new
 import java.util.HashMap;
 import java.util.Map;
 
 public class SlotMap {
 ////<keycode , SLOT>
+
+    public SlotMap(){
+
+    }
+
+    private String pathName = "vendingmachine.csv";
+
     private Map<String, Slot> slotMap = new HashMap<>();
+    public String line = null;
 
-    public void addItem(Slot newSlot){
-        slotMap.put(newSlot.getCode(), newSlot); //Should create a map of the String of the KEYCODE and it's SLot
+    
+    public void setUpMachine() throws MalformedItemException {
+        File filename = new File(pathName);
+
+        try (Scanner fileReader = new Scanner(filename)) {
+            while (fileReader.hasNextLine()) {
+                line = fileReader.nextLine();
+                Slot slot = new Slot(line);
+
+                slotMap.put(slot.currentItem.getKeyCode(), slot);
+            }
+        } catch (FileNotFoundException e) {
+            throw new MalformedItemException();
+        }
     }
-    public Map<String, Item> getSlotMap() {
-        return slotMap;} // this creates a copy of our list
+
+    //GETTER
+    public Map<String, Slot> getSlotMap() {
+        return new HashMap<>(slotMap);} // this creates a copy of our list
+    //pass userinput from ui
+    public void takeOneOut(String userInput){
+        slotMap.get(userInput).inventory -= 1;
     }
-
-
-
+}
