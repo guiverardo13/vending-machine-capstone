b683fd90c6b83ff7fad61f8a6f2ce3668c2ff631
e-fanto <elise.m.fanto@gmail.com>
2023-06-10 09:43:00 -0400

    next menu loop

--- src/main/java/com/techelevator/Menu.java
+++ src/main/java/com/techelevator/Menu.java
@@ -14,6 +14,8 @@ public class Menu {
     String name;
 
 
+
+
     public void displayMain() {
 
         boolean isReadyToQuit = false;
@@ -32,6 +34,7 @@ public class Menu {
                 case "2":
                     userInput = ui.getUserInput();
                     this.displayPurchaseMenu();
+
                     break;
                 case "3":
                     isReadyToQuit = true;
@@ -43,27 +46,39 @@ public class Menu {
             }
 
         }while (!isReadyToQuit);
-
     }
+
+
+
     public void displayPurchaseMenu() {
         boolean isReadyToQuit = false;
-        System.out.println("\n...\n");
-        ui.displayPurchaseMenu();
-        while (!isReadyToQuit) {
-            if (userInput.equals("1")) {
-                ui.displayFeedMoney();
-                userInput = ui.getUserInput();
-                ////I want to put the purchas logic into the PurchaseOption Class
-            }
-            if (userInput.equals("2")) {
-                ///purchase logic
-                System.out.println("trying to buy!"); //for now
-            }
-            if (userInput.equals("3")) {
+        do{
+            System.out.println("\n...\n");
+            ui.displayPurchaseMenu();
+            userInput = ui.getUserInput();
+            if (userInput.equals("3")){
                 isReadyToQuit = true;
-
             }
-        }
+            switch (userInput){
+                case "1":
+                    ui.displayFeedMoney();
+                    System.out.println("====more to come====");
+                    ////Call a method from purchase options CLASS
+                    break;
+                case "2":
+                    ui.displaySelectProduct();
+                    ////Call a method from purchase options CLASS
+
+                    break;
+                case "3":
+                    System.out.println("--going back to main menue--");
+                    isReadyToQuit = true;
+                    break;
+                default:
+                    ui.displayIncorrect();
+                    break;
+            }
+        } while (!isReadyToQuit);
 
 
     }
